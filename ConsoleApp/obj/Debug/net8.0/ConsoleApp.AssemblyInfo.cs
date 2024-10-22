06ab5666504887e95f23cd0325240537e2c06e8f
LeandroFrancois <leandrovidotofrancois@gmail.com>
2024-10-21 21:07:53 -0300

    testte branch
    add comentario pra teste

--- ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
+++ ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f0fed1cd32d115e042b8f92eef76b9a1ef4b8a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92a79be9728a31ff895d2866015131c83f8be139")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
