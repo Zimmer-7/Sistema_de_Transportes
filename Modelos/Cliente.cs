49d0ada5d528373745b18aff6b1421f66d432fbc
Gabriel Zimmer <gt3zimmer@gmail.com>
2024-11-20 18:18:54 -0300

    Primeiro commit v2

---
+++ Modelos/Cliente.cs
@@ -0,0 +1,40 @@
+namespace Modelos;
+
+public class Cliente
+{
+    public Cliente() {}
+    public Cliente(string nome, string email, string telefone)
+    {
+        Nome = nome;
+        Email = email;
+        Telefone = telefone;
+    }
+
+    public int ?IdCliente { get; set; }
+    public string Nome { get; set; }
+    public string Email { get; set; }
+    public string Telefone { get; set; }
+
+    public override bool Equals(object obj)
+    {   
+        if (obj == null || GetType() != obj.GetType())
+        {
+            return false;
+        }
+        
+        var other = (Cliente)obj;
+
+        return base.Equals(IdCliente.HasValue && other.IdCliente.HasValue 
+                            && IdCliente == other.IdCliente);
+    }
+
+    public override int GetHashCode()
+    {
+        return IdCliente.HasValue ? IdCliente.GetHashCode() : 0;
+    }
+
+    public override string ToString()
+    {
+        return $"[{IdCliente}, {Nome}, {Email}, {Telefone}]"; 
+    }
+}
