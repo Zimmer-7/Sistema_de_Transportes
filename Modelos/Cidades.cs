b7c92e4d2793711b90da184d9be93de2e04d0fb2
LeandroFrancois <leandrovidotofrancois@gmail.com>
2025-01-09 14:53:16 -0300

    classes
    Criei umas classes que vamos usar, para construir o cadastro e as utilidades

---
+++ Modelos/Cidades.cs
@@ -0,0 +1,42 @@
+namespace Modelos.Cidades
+{
+    public class Cidade
+    {
+        public Cidade() {}
+
+        public Cidade(string nome, string estado, string pais)
+        {
+            Nome = nome;
+            Estado = estado;
+            Pais = pais;
+        }
+
+        public int? IdCidade { get; set; }
+        public string Nome { get; set; }
+        public string Estado { get; set; }
+        public string Pais { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            var other = (Cidade)obj;
+
+            return base.Equals(IdCidade.HasValue && other.IdCidade.HasValue
+                                && IdCidade == other.IdCidade);
+        }
+
+        public override int GetHashCode()
+        {
+            return IdCidade.HasValue ? IdCidade.GetHashCode() : 0;
+        }
+
+        public override string ToString()
+        {
+            return $"[{IdCidade}, {Nome}, {Estado}, {Pais}]";
+        }
+    }
+}
---
+++ Modelos/Login.cs
@@ -0,0 +1,40 @@
+namespace Modelos.Login
+{
+    public class Login
+    {
+        public Login() {}
+
+        public Login(string email, string senha)
+        {
+            Email = email;
+            Senha = senha;
+        }
+
+        public int? IdLogin { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            var other = (Login)obj;
+
+            return base.Equals(IdLogin.HasValue && other.IdLogin.HasValue
+                                && IdLogin == other.IdLogin);
+        }
+
+        public override int GetHashCode()
+        {
+            return IdLogin.HasValue ? IdLogin.GetHashCode() : 0;
+        }
+
+        public override string ToString()
+        {
+            return $"[{IdLogin}, {Email}]";
+        }
+    }
+}
